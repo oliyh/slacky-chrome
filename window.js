b97d0aa67210d079c303afc7555cda50c6a660f3
Oliy <github@oliy.co.uk>
2015-09-06 17:19:44 +0100

    Initial commit

---
+++ window.js
@@ -0,0 +1,27 @@
+memePattern = new RegExp('\/meme$', 'i');
+
+function attachDomEventListeners() {
+   $('textarea, input').keyup(function(event) {
+      var userText = $(event.target).val();
+      var textJustTyped = userText.substring(0, doGetCaretPosition(event.target));
+      if (memePattern.test(textJustTyped)) {
+        console.log("Emitting memeDetected event");
+         //self.port.emit('memeDetected', $(event.target).attr('id'));
+      }
+   });
+}
+
+function attachSlackyEventListeners() {
+   //self.port.on('memeGenerated', function(target, memeUrl) {
+   //   var target = $('#' + target);
+   //   target.val(target.val().replace(memePattern, memeUrl));
+   //});
+}
+
+function init() {
+   console.log("Hello world, slacky is here!");
+   attachDomEventListeners();
+   attachSlackyEventListeners();
+}
+
+init();
