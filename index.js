ab9d32783145df4c82da4f7dad22e13e72ff414e
Oliver Hine <git@oliy.co.uk>
2015-09-17 08:59:56 +0100

    Adding a demo page for testing

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ chrome.runtime.onMessage.addListener(
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
-                
+
             sendResponse({memeUrl: "http://static.fjcdn.com/pictures/It+s+an+old+meme+how+i+feel+when+i+see_e83de6_3481193.jpg",
                    target: request.target});
-                   chrome.tabs.create({url: chrome.extension.getURL('slacky-panel.html')}, 
+                   chrome.tabs.create({url: chrome.extension.getURL('slacky-panel.html')},
                    function(tab) {
             // After the tab has been created, open a window to inject the tab
             chrome.windows.create({
                 tabId: tab.id,
                 type: 'popup',
                 focused: true
-                // incognito, top, left, ...
+              // incognito, top, left, ...
             });
         });;
   });
-  
+
   console.log('background page loaded');
